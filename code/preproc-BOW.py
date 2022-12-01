79b5cc09f69b3fc2475a95b0e7d4484a42a00cf5
El-BG-1970 <elouan.gros.fr@gmail.com>
2022-12-01 21:56:43 +0100

    generate bag of words vector for hashtags

--- code/preproc-BOW.py
+++ code/preproc-BOW.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from pprint import pprint
 
 def gen_BOW(data):
@@ -32,7 +33,6 @@ def gen_BOW(data):
         
     return shortBOW, proc_BOWs
 
-
 def gen_BOW2(data):
     """returns the list of words in the bag of words"""
     hashtags = [h.strip("[]").split(", ") for h in data['hashtags']]
@@ -47,8 +47,8 @@ def gen_BOW2(data):
             else:
                 BOW[h] = 1
 
-    return map(lambda x: x[0], list(sorted(BOW.items(), key=lambda item: item[1])[-100:]))
-    
+    return list(map(lambda x: x[0], list(sorted(BOW.items(), key=lambda item: item[1])[-100:])))
+
 def sentence_BOW(data, BOW):
     hashtags = h.strip("[]").split(", ")
     return dict(filter(lambda i: i[0] in shortBOW, B.items()))
@@ -57,9 +57,15 @@ def sentence_BOW2(data, BOW):
     """returns bit encoding of hashtag presence"""
     return np.array([1 if i in data else 0 for i in BOW])
     
+
+def gen_BOW_data(data):
+    BOW = gen_BOW2(data)
+    X = [sentence_BOW2(i.strip('[]').split(", "), BOW) for i in data['hashtags']]
+    Y = np.floor(np.log(data["retweets_count"]+1))
+    return X, Y 
+
 if __name__ == "__main__":
     train_data = pd.read_csv("../data/train.csv")
-    tmp = gen_BOW(train_data)
-    pprint(tmp[0])
-    pprint(tmp[1][-100:])
-    
+    tmp = gen_BOW_data(train_data)
+    pprint(tmp[0][-10:])
+    pprint(tmp[1][-10:])
