cdc1d58c49fb15b011f06b918266eaf8860aa7e2
“jetlime” <paul.houssel@yahoo.de>
2022-12-02 15:24:56 +0100

    clean-up

--- code/preproc-BOW.py
+++ code/preproc-BOW.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import numpy as np
 from pprint import pprint
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.decomposition import PCA
+from sklearn.decomposition import NMF
+import matplotlib.pyplot as plt 
+
+
 
 def gen_BOW(data):
     """generate bag of words"""
@@ -33,7 +39,7 @@ def gen_BOW(data):
         
     return shortBOW, proc_BOWs
 
-def gen_BOW2(data, tail=None):
+def gen_BOW2(data, tail=None, descriminate=None):
     """returns the list of words in the bag of words"""
     hashtags = [h.strip("[]").split(", ") for h in data['hashtags']]
     BOW = {}
@@ -49,6 +55,8 @@ def gen_BOW2(data, tail=None):
 
     if tail is not None:
         return list(map(lambda x: x[0], list(sorted(BOW.items(), key=lambda item: item[1]))[-tail:]))
+    if descriminate is not None:
+        return list(map(lambda x: x[0], list(sorted(filter(descriminate, BOW.items()), key=lambda item: item[1]))))
     return list(map(lambda x: x[0], list(sorted(BOW.items(), key=lambda item: item[1]))))
 
 def sentence_BOW(data, BOW):
@@ -58,20 +66,51 @@ def sentence_BOW(data, BOW):
 def sentence_BOW2(data, BOW):
     """returns bit encoding of hashtag presence"""
     return np.array([1 if i in data else 0 for i in BOW])
-    
+
+def batch_sentence_BOW2(data, BOW):
+    ret = np.zeros((data.shape[0], len(BOW)))
+    for i in range(data.shape[0]):
+        for j in data[i].strip("[]").split(", "):
+            if j in BOW:
+                ret[i, BOW.index(j)] = 1
+
+    return ret
 
 def gen_BOW_data(data):
-    BOW = gen_BOW2(data, 100)
+    BOW = gen_BOW2(data, None)
     X = [sentence_BOW2(i.strip('[]').split(", "), BOW) for i in data['hashtags']]
     Y = np.floor(np.log(data["retweets_count"]+1))
     return X, Y
 
 def gen_BOW_dataframe(data):
-    BOW = gen_BOW2(data, 100)
-    X = [sentence_BOW2(i.strip('[]').split(", "), BOW) for i in data['hashtags']]
+    # Only keep the hashtags appearing more than 5 times
+    BOW = gen_BOW2(data, None, descriminate=lambda x:x[1]>5)
+    print("BOW is done")
+    print(len(BOW))
+    X = batch_sentence_BOW2(data["hashtags"], BOW)
+    #X = [sentence_BOW2(i.strip('[]').split(", "), BOW) for i in data['hashtags']]
     df = pd.DataFrame(X, columns=BOW)
     df["retweets_count"] = data["retweets_count"]
     return df
+
+def gen_PCA(df):
+    pca = PCA(n_components=1)
+    X = df.drop(['retweets_count'], axis=1)
+    pca_features = pca.fit_transform(X, )
+    return pca_features, pca
+
+def gen_lda(df):
+    clf = LDA(n_components=3)
+    X = df.drop(['retweets_count'], axis=1)
+    df_new = clf.fit_transform(X, df["retweets_count"])
+    return df_new, clf
+
+def gen_nmf(df):
+    clf = LDA(n_components=10)
+    X = df.drop(['retweets_count'], axis=1)
+    df_new = clf.fit_transform(X, df["retweets_count"])
+    return df_new, clf
+
 
 if __name__ == "__main__":
     train_data = pd.read_csv("../data/train.csv")
@@ -80,5 +119,53 @@ if __name__ == "__main__":
     # pprint(tmp[1][-10:])
     # pprint(np.max(tmp[1]))
     df = gen_BOW_dataframe(train_data)
-    pprint(df)
-    df.to_csv("../data/BOW.csv")
+    print(df.shape)
+    df.to_csv("../data/BOW-full.csv")
+    #df = pd.read_csv('../data/BOW.csv')
+
+    # NMF
+    #df = pd.read_csv('../data/BOW.csv')
+    df_new,nmf = gen_nmf(df)
+
+    plt.bar(
+    range(1,len(nmf.explained_variance_ratio_)+1),
+    nmf.explained_variance_ratio_
+    )
+    print(nmf.coef_)
+
+    plt.xlabel('nmf Feature')
+    plt.ylabel('Explained variance')
+    plt.title('Feature Explained Variance')
+    plt.savefig('../figs/hashtags/nmf.png')
+    '''
+    # PCA
+    df = pd.read_csv('../data/BOW.csv')
+    df_new,pca = gen_PCA(df)
+
+    plt.bar(
+    range(1,len(pca.explained_variance_)+1),
+    pca.explained_variance_
+    )
+
+
+    plt.xlabel('PCA Feature')
+    plt.ylabel('Explained variance')
+    plt.title('Feature Explained Variance')
+    plt.savefig('../figs/hashtags/pca.png')
+
+
+    # LDA
+    df = pd.read_csv('../data/BOW.csv')
+    df_new,lda = gen_lda(df)
+
+    plt.bar(
+    range(1,len(lda.explained_variance_ratio_)+1),
+    lda.explained_variance_ratio_
+    )
+    print(lda.coef_)
+
+    plt.xlabel('lda Feature')
+    plt.ylabel('Explained variance')
+    plt.title('Feature Explained Variance')
+    plt.savefig('../figs/hashtags/lda.png')
+    '''
