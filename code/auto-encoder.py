125946c48ab2cd58e953a84c7e712071574fbc56
“jetlime” <paul.houssel@yahoo.de>
2022-12-03 09:52:25 +0100

    improve autoencode to gen 5 features

--- code/auto-encoder.py
+++ code/auto-encoder.py
@@ -7,6 +7,20 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from preproc_BOW import gen_BOW_dataframe
 
+
+import os
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+from numpy.random import seed
+from sklearn.preprocessing import minmax_scale
+from sklearn.model_selection import train_test_split
+from keras.layers import Input, Dense
+from keras.models import Model
+
+callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+
+
 if __name__ == "__main__":
     train_data = pd.read_csv("../data/train.csv")
     # tmp = gen_BOW_data(train_data)
@@ -19,9 +33,9 @@ if __name__ == "__main__":
     X_train, x_test = train_test_split(df, test_size=0.2, random_state=45)
     print(X_train.shape)
     print(x_test.shape)
-
+    '''
     hidden_size = 100
-    latent_size = 3
+    latent_size = 2
 
     input_layer = layers.Input(shape = X_train.shape[1:])
     flattened = layers.Flatten()(input_layer)
@@ -42,7 +56,67 @@ if __name__ == "__main__":
         
     
     autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
-    history = autoencoder.fit(X_train, X_train, epochs=5, batch_size=100, validation_data = (x_test, x_test))
+    history = autoencoder.fit(X_train, X_train, epochs=1, batch_size=150, validation_data = (x_test, x_test), callbacks=[callback])
+
+    '''
+
+    # define the number of features
+    ncol = X_train.shape[1]
+
+    encoding_dim = 5
+
+    input_dim = Input(shape = (ncol, ))
+
+    # Encoder Layers
+    encoded7 = Dense(1250, activation = 'relu')(input_dim)
+    encoded8 = Dense(900, activation = 'relu')(encoded7)
+    encoded9 = Dense(700, activation = 'relu')(encoded8)
+    encoded10 = Dense(400, activation = 'relu')(encoded9)
+    encoded11 = Dense(200, activation = 'relu')(encoded10)
+    encoded12 = Dense(100, activation = 'relu')(encoded11)
+    encoded13 = Dense(encoding_dim, activation = 'relu')(encoded12)
+
+    # Decoder Layers
+    decoded1 = Dense(100, activation = 'relu')(encoded13)
+    decoded2 = Dense(400, activation = 'relu')(decoded1)
+    decoded3 = Dense(800, activation = 'relu')(decoded2)
+    decoded4 = Dense(1000, activation = 'relu')(decoded3)
+    decoded5 = Dense(ncol, activation = 'sigmoid')(decoded4)
+
+    # Combine Encoder and Deocder layers
+    autoencoder = Model(inputs = input_dim, outputs = decoded5)
+
+    # Compile the Model
+    autoencoder.compile(optimizer = 'adadelta', loss = 'binary_crossentropy')
+
+
+    history = autoencoder.fit(X_train, X_train, epochs = 10, batch_size = 150, shuffle = False, validation_data = (x_test, x_test), callbacks=[callback])
+
+
+    # Use Encoder level to reduce dimension of train and test data¶
+    encoder = Model(inputs = input_dim, outputs = encoded13)
+    encoded_input = Input(shape = (encoding_dim, ))
+
+    encoded_train = pd.DataFrame(encoder.predict(X_train))
+    encoded_train = encoded_train.add_prefix('hastag_encoded_')
+
+    encoded_test = pd.DataFrame(encoder.predict(x_test))
+    encoded_test = encoded_test.add_prefix('hastag_encoded_')
+
+
+
+    print(encoded_train.shape)
+    encoded_train.head()
+
+    print(encoded_test.shape)
+    encoded_test.head()
+
+
+
+    encoded_train.to_csv('train_encoded.csv', index=False)
+    encoded_test.to_csv('test_encoded.csv', index=False)
+
+
 
 
     fig, axs = plt.subplots(figsize=(15,15))
