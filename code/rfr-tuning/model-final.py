cdcfbe1faf6b511cc67aca3f5488e33086ef3721
“jetlime” <paul.houssel@yahoo.de>
2022-12-07 16:20:08 +0100

    updtate nn

--- code/rfr-tuning/model-final.py
+++ code/rfr-tuning/model-final.py
@@ -51,6 +51,7 @@ if __name__ == "__main__":
 
 		# We want to make sure that all predictions are non-negative integers
 		y_pred = [int(value) if value >= 0 else 0 for value in y_pred]
+		mini = result_pred
 		if result_pred<mini:
 			print("Found better result, saving the prediction to csv file")
 			# Dump the results into a file that follows the required Kaggle template
